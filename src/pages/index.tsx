8157b83ab46f5816914297e4204ced2a02cd34ef
paulobeckman <beckmam.paulo@hotmail.com>
2021-09-30 12:39:32 -0300

    update pade home error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,6 +30,18 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps) {
+  function date(timestamp){
+    const date = new Date(timestamp)
+    const meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul","Ago","Set","Out","Nov","Dez"];
+
+    const year = date.getUTCFullYear()
+    const month = meses[date.getUTCMonth()]
+    const day = `0${date.getUTCDate()}`.slice(-2)
+
+    return {
+        format: `${day} ${month} ${year}`
+    }
+  }
   return (
     <>
       <Head>
@@ -38,25 +50,24 @@ export default function Home({ postsPagination }: HomeProps) {
       <main className={commonStyles.container}>
         <div className={styles.posts}>
           {postsPagination.results.map(post => (
-            <Link href={`/posts/${post.uid}`} >
-              <a key={post.uid}>
-                 <strong>{post.data.title}</strong>
-                 <p>{post.data.subtitle}</p>
-
-                 <footer>
-                   <section>
+            <Link key={post.uid} href={`/posts/${post.uid}`} >
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <footer>
+                  <section>
                     <FaRegCalendar />
-                    <time>{post.first_publication_date}</time>
-                   </section>
-                   <section>
+                    <time>{date(post.first_publication_date).format}</time>
+                  </section>
+                  <section>
                     <FaRegUser />
                     <p>{post.data.author}</p>
-                   </section>
-                 </footer>
+                  </section>
+                </footer>
               </a>
             </Link>
           ))}
-        <a href="">Carregar mais posts</a>
+        <a href={postsPagination.next_page}>Carregar mais posts</a>
         </div>
       </main>
     </>
@@ -70,7 +81,8 @@ export const getStaticProps: GetStaticProps = async () => {
     Prismic.predicates.at('document.type', 'posts')
   ], {
     fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
-    pageSize: 2,
+    pageSize: 1,
+    page: 1,
   })
 
   // console.log(JSON.stringify(response, null, 2))
@@ -78,11 +90,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const results = response.results.map(post => {
     return {
       uid: post.uid,
-      first_publication_date: new Date(post.first_publication_date).toLocaleDateString('nl', {
-        day: '2-digit',
-        month: `short`,
-        year: 'numeric'
-      }),
+      first_publication_date: post.first_publication_date,
       data: {
         title: post.data.title,
         subtitle: post.data.subtitle,
@@ -91,8 +99,11 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   })
 
+  const next_page = response.next_page
+
   const postsPagination ={
-    results
+    results,
+    next_page
   }
 
   return {
